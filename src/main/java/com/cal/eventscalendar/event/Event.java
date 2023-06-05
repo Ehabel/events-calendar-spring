e066958008f8d238778da9f29234bf0ed5bc2b27
Ehabel <eelcheikh@hotmail.com>
2023-06-05 17:16:43 +1000

    Returns id with other info

--- src/main/java/com/cal/eventscalendar/event/Event.java
+++ src/main/java/com/cal/eventscalendar/event/Event.java
@@ -40,6 +40,14 @@ public class Event {
 
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getEventName() {
         return eventName;
     }
